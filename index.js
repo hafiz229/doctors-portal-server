b217890f09519c41f918707c9c4d37ae6241a5fc
Hafiz <hasnathafiz229>
2021-11-09 11:53:51 +0600

    remove locale date string

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function run() {
     // get all appointments from the database
     app.get("/appointments", verifyToken, async (req, res) => {
       const email = req.query.email;
-      const date = new Date(req.query.date).toLocaleDateString();
+      const date = req.query.date;
       const query = { email: email, date: date };
       const cursor = appointmentsCollection.find(query);
       const appointments = await cursor.toArray();
